0be5771eb7716eb784e44d92de9596273a8a5176
unknown <shashank080311@gmail.com>
2021-02-11 18:43:18 +0530

    mongo cloud added

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
@@ -7,8 +8,22 @@ const app = express();
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
-mongoose.connect("mongodb://localhost:27017/mainDB", { useUnifiedTopology: true, useNewUrlParser: true });
+mongoose.connect(`mongodb+srv://${process.env.ADMIN}:${process.env.PASSWORD}@cluster0.eyjhl.mongodb.net/projectDB`, { useUnifiedTopology: true, useNewUrlParser: true });
 
+const eventSchema = new mongoose.Schema({
+    placeName: String,
+    hostings:Number,
+    id: String 
+});
+
+const Event = mongoose.model("Event", eventSchema);
+
+const event = new Event({
+    placeName: "kolkata",
+    hostings:"300",
+    id: "01"
+});
+event.save();
 app.get("/", (req,res)=>{
     res.json({
         status: "ok"
