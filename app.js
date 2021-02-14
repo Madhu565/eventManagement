999332286c83b07818632594f7c8e8e5cf5922a2
mayukh-B <rudrabagchi11@gmail.com>
2021-02-14 12:33:08 +0530

    routes made and connected between login-register

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
+const passport = require('passport');
+const expressSession = require('express-session');
+const passportLocalMongoose = require('passport-local-mongoose');
+
 
 const app = express();
 app.set('view engine', 'ejs');
@@ -10,11 +14,26 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 mongoose.connect(`mongodb+srv://${process.env.ADMIN}:${process.env.PASSWORD}@cluster0.eyjhl.mongodb.net/projectDB`, { useUnifiedTopology: true, useNewUrlParser: true });
 
+///////ORGANIZER SCHEMA
+const organiserSchema = new mongoose.Schema({
+    username: String,
+    password: String,
+    name: String
+});
+
+const Organiser = mongoose.model("Organiser", organiserSchema);
+
+////////
+
+
+
 const eventSchema = new mongoose.Schema({
     placeName: String,
     hostings:Number,
     id: String 
 });
+
+
 
 const Event = mongoose.model("Event", eventSchema);
 
@@ -28,6 +47,17 @@ app.get("/", (req,res)=>{
     res.json({
         status: "ok"
     })
+})
+
+app.get("/userLoginRegister", (req,res)=>{
+    res.render('userLoginRegister');
+})
+
+app.get("/users/login",(req,res)=>{
+    res.render('login');
+})
+app.get("/users/register",(req,res)=>{
+    res.render('register');
 })
 
 app.listen(3000 , ()=>{
